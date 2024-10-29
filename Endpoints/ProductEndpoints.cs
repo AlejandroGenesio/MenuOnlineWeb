5cca134644309e4df56dec1dbbd0be107c0a2451
Alejandro Genesio <angenesio@hotmail.com>
2024-10-29 14:48:22 +0100

    Organizing program.cs moving endpoints to a class

---
+++ Endpoints/ProductEndpoints.cs
@@ -0,0 +1,75 @@
+﻿using MenuOnlineUdemy.Entities;
+using MenuOnlineUdemy.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Runtime.CompilerServices;
+
+namespace MenuOnlineUdemy.Endpoints
+{
+    public static class ProductEndpoints
+    {
+        public static RouteGroupBuilder MapProducts(this RouteGroupBuilder group)
+        {
+
+            group.MapGet("/", GetProducts);
+
+            group.MapGet("/{id:int}", GetProductsById);
+
+            group.MapPost("/", CreateProduct);
+
+            group.MapPut("/{id:int}", UpdateProduct);
+
+            group.MapDelete("/{id:int}", DeleteProduct);
+
+            return group;
+        }
+
+        static async Task<Ok<List<Product>>> GetProducts(IRepositoryProducts repository)
+        {
+
+            var products = await repository.GetAll();
+            return TypedResults.Ok(products);
+        }
+
+        static async Task<Results<Ok<Product>, NotFound>> GetProductsById(IRepositoryProducts repository, int id)
+        {
+            var product = await repository.GetById(id);
+
+            if (product == null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            return TypedResults.Ok(product);
+        }
+
+        static async Task<Created<Product>> CreateProduct(Product product, IRepositoryProducts repository)
+        {
+            var id = await repository.Create(product);
+            return TypedResults.Created($"/products/{id}", product);
+        }
+
+        static async Task<Results<NoContent, NotFound>> UpdateProduct(int id, Product product, IRepositoryProducts repository)
+        {
+            var exists = await repository.IfExists(id);
+            if (!exists)
+            {
+                return TypedResults.NotFound();
+            }
+
+            await repository.Update(product);
+            return TypedResults.NoContent();
+        }
+
+        static async Task<Results<NotFound, NoContent>> DeleteProduct(int id, IRepositoryProducts repository)
+        {
+            var exists = await repository.IfExists(id);
+            if (!exists)
+            {
+                return TypedResults.NotFound();
+            }
+
+            await repository.Delete(id);
+            return TypedResults.NoContent();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using MenuOnlineUdemy;
+using MenuOnlineUdemy.Endpoints;
 using MenuOnlineUdemy.Entities;
 using MenuOnlineUdemy.Repositories;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -27,67 +28,8 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
 
-var enpointsProducts = app.MapGroup("/products");
-
-enpointsProducts.MapGet("/", GetProducts);
-
-enpointsProducts.MapGet("/{id:int}", GetProductsById);
-
-enpointsProducts.MapPost("/", CreateProduct);
-
-enpointsProducts.MapPut("/{id:int}", UpdateProduct);
-
-enpointsProducts.MapDelete("/{id:int}", DeleteProduct);
+app.MapGroup("/products").MapProducts();
 
 // Middleware END
 
 app.Run();
-
-static async Task<Ok<List<Product>>> GetProducts (IRepositoryProducts repository)
-{
-
-    var products = await repository.GetAll();
-    return TypedResults.Ok(products);
-}
-
-static async Task<Results<Ok<Product>, NotFound>> GetProductsById(IRepositoryProducts repository, int id)
-{
-    var product = await repository.GetById(id);
-
-    if (product == null)
-    {
-        return TypedResults.NotFound();
-    }
-
-    return TypedResults.Ok(product);
-}
-
-static async Task<Created<Product>> CreateProduct(Product product, IRepositoryProducts repository)
-{
-    var id = await repository.Create(product);
-    return TypedResults.Created($"/products/{id}", product);
-}
-
-static async Task<Results<NoContent, NotFound>> UpdateProduct(int id, Product product, IRepositoryProducts repository)
-{
-    var exists = await repository.IfExists(id);
-    if (!exists)
-    {
-        return TypedResults.NotFound();
-    }
-
-    await repository.Update(product);
-    return TypedResults.NoContent();
-}
-
-static async Task<Results<NotFound, NoContent>> DeleteProduct(int id, IRepositoryProducts repository)
-{
-    var exists = await repository.IfExists(id);
-    if (!exists)
-    {
-        return TypedResults.NotFound();
-    }
-
-    await repository.Delete(id);
-    return TypedResults.NoContent();
-}
